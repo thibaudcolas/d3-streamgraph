09b635260d5c966661ce83936bf4217019b670a7
Thibaud Colas <thibaudcolas@gmail.com>
2016-03-02 15:04:31 +0200

    Now using time scale and X axis

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ const mount = document.querySelector('#mount');
 
 const width = mount.offsetWidth;
 const height = width / 3;
+const margin = { top: 0, bottom: 20, left: 0, right: 0 };
 
 const data = rawData.map(repo => {
     return repo.commitActivity.map((week, i) => {
         return {
             x: i + 1,
-            t: new Date(week.week),
+            t: new Date(week.week * 1000),
             y: week.total,
         };
     });
@@ -44,13 +45,13 @@ function bumpLayer(n) {
 // expand - normalize layers to fill the range [0,1].
 // zero - use a zero baseline, i.e., the y-axis.
 // number of samples per layer
-const stack = d3.layout.stack().offset('silhouette');
+const stack = d3.layout.stack().offset('zero');
 // const layers = stack(d3.range(n).map(() => bumpLayer(m)));
 const layers = stack(data);
 
 const scales = {
-    x: d3.scale.linear()
-        .domain([1, 52])
+    x: d3.time.scale()
+        .domain(d3.extent(data[0], d => d.t))
         .range([0, width]),
 
     y: d3.scale.linear()
@@ -62,18 +63,37 @@ const scales = {
     color: d3.scale.category20(),
 };
 
+const axis = {
+    x: d3.svg.axis()
+        .scale(scales.x)
+        .orient('bottom'),
+};
+
 const area = d3.svg.area()
     .interpolate('basis')
-    .x(d => scales.x(d.x))
+    .x(d => scales.x(d.t))
     .y0(d => scales.y(d.y0))
     .y1(d => scales.y(d.y0 + d.y));
 
 const svg = d3.select(mount).append('svg')
-    .attr('width', width)
-    .attr('height', height);
+    .attr('class', 'chart')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-svg.selectAll('path')
+const areas = svg.append('g').attr('class', 'areas');
+
+svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height})`);
+
+svg.select('.axis.x')
+    .transition()
+    .call(axis.x);
+
+areas.selectAll('path')
     .data(layers)
     .enter().append('path')
     .attr('d', area)
-    .style('fill', () => scales.color(Math.random()));
+    .style('fill', (d, i) => scales.color(rawData[i].full_name));
